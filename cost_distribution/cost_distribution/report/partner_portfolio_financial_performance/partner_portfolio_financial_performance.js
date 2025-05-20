247f36c9f153b0becbc786e32fe01791d03aeb3c
yazan01 <119818390+yazan01@users.noreply.github.com>
2025-05-20 15:43:26 +0300

    Update partner_portfolio_financial_performance.js

--- cost_distribution/cost_distribution/report/partner_portfolio_financial_performance/partner_portfolio_financial_performance.js
+++ cost_distribution/cost_distribution/report/partner_portfolio_financial_performance/partner_portfolio_financial_performance.js
@@ -22,7 +22,7 @@ frappe.query_reports["Partner portfolio Financial Performance"] = {
         {
             "fieldname": "project",
             "label": ("Project"),
-            "fieldtype": "Link",
+            "fieldtype": "Table MultiSelect",
             "options": "Project"
         },
         {
