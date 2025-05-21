5b748480cce2dfdf038a2ac0dae8d9d888a92ea3
yazan01 <119818390+yazan01@users.noreply.github.com>
2025-05-21 14:02:16 +0300

    Add files via upload

---
+++ cost_distribution/cost_distribution/report/partner_portfolio_financial_performance_par_project/partner_portfolio_financial_performance_par_project.js
@@ -0,0 +1,74 @@
+
+frappe.query_reports["Partner Portfolio Financial Performance Par Project"] = {
+    "filters": [
+        {
+            "fieldname": "partner",
+            "label": ("Partner"),
+            "fieldtype": "Link",
+            "options": "Employee",
+            "get_query": function() {
+                return {
+                    "filters": [
+                        ["designation", "in", ["Partner", "CEO"]]
+                    ]
+                };
+            }
+        },
+        {
+            "fieldname": "project_type",
+            "label": ("Project Type"),
+            "fieldtype": "Link",
+            "options": "Project Type"
+        },
+        {
+            "fieldname": "portfolio_category",
+            "label": ("Portfolio Category"),
+            "fieldtype": "Select",
+            "options": "New\nOld",
+            "default": "New"
+        },
+        {
+            "fieldname": "project",
+            "label": __("Project"),
+            "fieldtype": "MultiSelectList",
+            "get_data": function (txt) {
+                const partner = frappe.query_report.get_filter_value("partner");
+                const project_type = frappe.query_report.get_filter_value("project_type");
+                const portfolio_category = frappe.query_report.get_filter_value("portfolio_category");
+        
+                if (!partner) {
+                    frappe.msgprint("Please select a Partner first");
+                    return [];
+                }
+        
+                return frappe.call({
+                    method: "cost_distribution.cost_distribution.report.partner_portfolio_financial_performance.partner_portfolio_financial_performance.get_projects_by_partner",
+                    args: {
+                        partner: partner,
+                        txt: txt || "",
+                        project_type: project_type || null,
+                        portfolio_category: portfolio_category || null
+                    },
+                }).then(r => {
+                    return r.message || [];
+                });
+            }
+        },
+        {
+            "fieldname": "aggregated",
+            "label": ("Aggregated"),
+            "fieldtype": "Check",
+            "default": 0
+        },
+        {
+            "fieldname": "from_date",
+            "label": ("From Date"),
+            "fieldtype": "Date"
+        },
+        {
+            "fieldname": "to_date",
+            "label": ("To Date"),
+            "fieldtype": "Date"
+        }
+    ]
+};
