98d0cec697e6ecb5db91e97229e14da518fa134b
yazan01 <119818390+yazan01@users.noreply.github.com>
2025-05-21 10:48:49 +0300

    Update partner_portfolio_financial_performance.js

--- cost_distribution/cost_distribution/report/partner_portfolio_financial_performance/partner_portfolio_financial_performance.js
+++ cost_distribution/cost_distribution/report/partner_portfolio_financial_performance/partner_portfolio_financial_performance.js
@@ -32,19 +32,24 @@ frappe.query_reports["Partner portfolio Financial Performance"] = {
             "fieldtype": "MultiSelectList",
             "get_data": function (txt) {
                 const partner = frappe.query_report.get_filter_value("partner");
+                const project_type = frappe.query_report.get_filter_value("project_type");
+                const portfolio_category = frappe.query_report.get_filter_value("portfolio_category");
+        
                 if (!partner) {
                     frappe.msgprint("Please select a Partner first");
                     return [];
                 }
         
                 return frappe.call({
-                    method: "cost_distribution.cost_distribution.report.partner_portfolio_financial_performance.partner_portfolio_financial_performance.get_projects_by_partner",  // عدّل المسار حسب موقع تقريرك
+                    method: "cost_distribution.cost_distribution.report.partner_portfolio_financial_performance.partner_portfolio_financial_performance.get_projects_by_partner",
                     args: {
                         partner: partner,
-                        txt: txt || ""
+                        txt: txt || "",
+                        project_type: project_type || null,
+                        portfolio_category: portfolio_category || null
                     },
                 }).then(r => {
-                    return r.message;
+                    return r.message || [];
                 });
             }
         },
