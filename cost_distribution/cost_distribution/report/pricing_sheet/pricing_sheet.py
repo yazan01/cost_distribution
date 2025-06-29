0e4fa091cfa254a03373b37f676f69fe675aa76a
yazan01 <119818390+yazan01@users.noreply.github.com>
2025-06-29 10:13:21 +0300

    Update pricing_sheet.py

--- cost_distribution/cost_distribution/report/pricing_sheet/pricing_sheet.py
+++ cost_distribution/cost_distribution/report/pricing_sheet/pricing_sheet.py
@@ -1,26 +1,25 @@
 import frappe
 
 def execute(filters=None):
+    filters = filters or {}
     project = filters.get("project")
-    
 
-    conditions = "1=1"
+    raw_data = []
+
     if project:
-        conditions += f" AND project = '{project}'"
-  
+        conditions = f"project = '{project}'"
+        raw_data = frappe.db.sql(f"""
+            SELECT 
+                `year`, project, parent, ctc
+            FROM 
+                `tabLevel Rate`
+            WHERE 
+                {conditions}
+            ORDER BY 
+                `year`, parent
+        """, as_dict=True)
 
-    data = frappe.db.sql(f"""
-        SELECT 
-            `year`, project, parent, ctc
-        FROM 
-            `tabLevel Rate`
-        WHERE 
-            {conditions}
-        ORDER BY 
-            `year` , parent 
-    """, as_dict=True)
-
-    # If no pricing sheet Project is null
+    # إذا لم يتم تمرير project أو النتيجة فارغة، نحاول الحصول على النتائج where project is null
     if not raw_data:
         raw_data = frappe.db.sql("""
             SELECT 
@@ -33,7 +32,7 @@ def execute(filters=None):
                 `year`, parent
         """, as_dict=True)
 
-    
+    # تجهيز البيانات
     data = []
     for row in raw_data:
         data.append({
@@ -41,11 +40,11 @@ def execute(filters=None):
             "project": row.project,
             "level": row.parent,
             "ctc": row.ctc
-        })   
+        })
 
     columns = [
         {"label": "Year", "fieldname": "year", "fieldtype": "Data", "width": 180},
-        {"label": "Project", "fieldname": "project", "fieldtype": "Link", "option": "Project", "width": 180},
+        {"label": "Project", "fieldname": "project", "fieldtype": "Link", "options": "Project", "width": 180},
         {"label": "Level", "fieldname": "level", "fieldtype": "Data", "width": 180},
         {"label": "CTC", "fieldname": "ctc", "fieldtype": "Float", "width": 180},
     ]
