ada3bc47522a6642c759391fbe57a33076b3447e
yazan01 <119818390+yazan01@users.noreply.github.com>
2025-11-13 15:22:25 +0300

    Add files via upload

---
+++ cost_distribution/cost_distribution/doctype/employee_assumptions/employee_assumptions.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2025, Yazan Hamdan
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class EmployeeAssumptions(Document):
+    return
+
+
