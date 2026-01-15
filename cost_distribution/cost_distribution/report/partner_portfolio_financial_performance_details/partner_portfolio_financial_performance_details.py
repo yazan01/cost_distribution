014aa9bcae9b9ec2a6197b6938a54ec4e40004b7
yazan01 <119818390+yazan01@users.noreply.github.com>
2026-01-15 12:01:57 +0300

    Update partner_portfolio_financial_performance_details.py

--- cost_distribution/cost_distribution/report/partner_portfolio_financial_performance_details/partner_portfolio_financial_performance_details.py
+++ cost_distribution/cost_distribution/report/partner_portfolio_financial_performance_details/partner_portfolio_financial_performance_details.py
@@ -41,6 +41,8 @@ def get_project_data(filters):
     to_date_filter = filters.get("to_date")
     group_filter = filters.get("group")
     data_type = filters.get("data_type")
+    persentage = filters.get("persentage")
+    
 
     if project_filter:
         if isinstance(project_filter, list):
@@ -86,7 +88,13 @@ def get_project_data(filters):
         WHERE {where_sql}
     """, params, as_dict=True)
 
-    project_percentages = {p["project_id"]: float(p["percentage"] or 0) / 100 for p in all_projects}
+
+    if persentage:
+        project_percentages = {p["project_id"]: float(p["percentage"] or 0) / 100 for p in all_projects}
+    else:
+        project_percentages = {p["project_id"]: float(100) / 100 for p in all_projects}
+    
+    # project_percentages = {p["project_id"]: float(p["percentage"] or 0) / 100 for p in all_projects}
     project_ids = list(project_percentages.keys())
 
     if not project_ids:
