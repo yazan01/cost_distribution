46a9b4fd605d36168635c1915cf1717f7b04894d
yazan01 <119818390+yazan01@users.noreply.github.com>
2026-01-15 14:26:27 +0300

    Update partner_portfolio_financial_performance_par_project.js

--- cost_distribution/cost_distribution/report/partner_portfolio_financial_performance_par_project/partner_portfolio_financial_performance_par_project.js
+++ cost_distribution/cost_distribution/report/partner_portfolio_financial_performance_par_project/partner_portfolio_financial_performance_par_project.js
@@ -33,8 +33,8 @@ frappe.query_reports["Partner Portfolio Financial Performance Par Project"] = {
         {
             "fieldname": "portfolio_category",
             "label": ("Portfolio Category"),
-            "fieldtype": "Select",
-            "options": "\nNew\nOld",
+            "fieldtype": "Link",
+            "options": "Portfolio Category",
             "default": "New",
             "on_change": function() {
                 // تفريغ فلتر المشاريع عند تغيير فئة المحفظة
